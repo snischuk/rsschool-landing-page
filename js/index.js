198989da211c4281a6e30cf83cee13e6efe648b5
Max Snischuk <m.snischuk@gmail.com>
2026-09-17 14:21:56 +0300

    feat: add header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 console.log('Hello, world!');
+// function initBurgerMenu() {
+//     const burger = document.querySelector('.header__burger');
+//     const links = document.querySelector('.header__links-wrapper');
+//     const body = document.querySelector('body');
+
+//     function clickedLinkHandler(event){
+//         if(!burger.classList.contains('header__burger--active')) return;
+//         if (event.target.tagName !== 'A') return;
+
+//         body.classList.remove('js-lock');
+//         burger.classList.remove('header__burger--active');
+//         links.classList.remove('header__links-wrapper--active');
+//     }
+
+//     function toggleBurgerHandler() {
+//         body.classList.toggle('js-lock');
+//         burger.classList.toggle('header__burger--active');
+//         links.classList.toggle('header__links-wrapper--active');
+
+//         links.addEventListener('click', clickedLinkHandler, { once: true })
+//     }
+    
+//     burger.addEventListener('click', toggleBurgerHandler);
+// }
+
+// document.addEventListener('DOMContentLoaded', initBurgerMenu)
